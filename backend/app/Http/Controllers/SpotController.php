4831d8734dce0e7543d16e1b31d7dc591f5bb8e3
AswinA <aswinarung2@gmail.com>
2022-12-21 05:00:44 +0700

    fix spot controller

--- backend/app/Http/Controllers/SpotController.php
+++ backend/app/Http/Controllers/SpotController.php
@@ -15,7 +15,8 @@ class SpotController extends Controller
     public function getSpot(Request $request)
     {
         $user = Society::where('login_tokens', $request->bearerToken())->first();
-        $spots = Spot::where('regional_id', $user->regional_id)->get();
+        $vaccinations = Vaccination::where('society_id', $user->id)->count();
+        $spots = Spot::all();
         $vaccines = Vaccine::all();
         $available_vaccines = [];
         foreach($spots as $spot){
@@ -24,7 +25,15 @@ class SpotController extends Controller
                 $exist = $vaccine_type ? 'true' : 'false';
                 $available_vaccines[$vaccine['name']] = $exist;
             }
+
+            if($spot['serve'] != $vaccinations){
+                $spot['unavailable'] = true;
+            }
+
+            $spot_vaccine = SpotVaccine::where('spot_id', $spot->id)->first();
+            $vaccine = Vaccine::find($spot_vaccine['vaccine_id']);
             $spot['available_vaccines'] = $available_vaccines;
+            $spot['vaccines'] = $vaccine;
         }
 
         return response()->json([
@@ -36,13 +45,19 @@ class SpotController extends Controller
     public function showSpot(Request $request, $id)
     {
         $spot = Spot::where('id', $id)->first();
+
         if($request->date){
             $date = $request->date;
         }else{
             $date = now();
         }
+
         $format_date = Carbon::parse($date)->format('F d, Y');
         $vaccination_count = Vaccination::where('date', $request->date)->where('spot_id', $id)->count();
+        $spot_vaccine = SpotVaccine::where('spot_id', $id)->first();
+        $vaccine = Vaccine::find($spot_vaccine['vaccine_id']);
+        $spot['vaccine'] = $vaccine;
+
         return response()->json([
             'data' => [
                 'date' => $format_date,
